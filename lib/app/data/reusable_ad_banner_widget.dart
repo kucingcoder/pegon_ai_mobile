5de8bc1af260b07a30bb8373fc8d8a95c76a3987
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2025-06-27 16:46:30 +0700

    refactor ReusableAdBannerWidget to use adUnitId from Variabels class; simplify ad loading logic

--- lib/app/data/reusable_ad_banner_widget.dart
+++ lib/app/data/reusable_ad_banner_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:pegon_ai_mobile/app/data/variabels.dart';
 
 class ReusableAdBannerWidget extends StatefulWidget {
-  final String adUnitId;
-
-  const ReusableAdBannerWidget({super.key, required this.adUnitId});
+  const ReusableAdBannerWidget({super.key});
 
   @override
   State<ReusableAdBannerWidget> createState() => _ReusableAdBannerWidgetState();
@@ -19,15 +18,11 @@ class _ReusableAdBannerWidgetState extends State<ReusableAdBannerWidget> {
     super.initState();
 
     _bannerAd = BannerAd(
-      adUnitId: widget.adUnitId,
+      adUnitId: Variabels.adUnitId,
       size: AdSize.banner,
-      request: AdRequest(),
+      request: const AdRequest(),
       listener: BannerAdListener(
-        onAdLoaded: (_) {
-          setState(() {
-            _isLoaded = true;
-          });
-        },
+        onAdLoaded: (_) => setState(() => _isLoaded = true),
         onAdFailedToLoad: (ad, error) {
           ad.dispose();
           debugPrint('Ad failed to load: ${error.message}');
@@ -44,14 +39,14 @@ class _ReusableAdBannerWidgetState extends State<ReusableAdBannerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    if (_isLoaded && _bannerAd != null) {
-      return SizedBox(
-        height: _bannerAd!.size.height.toDouble(),
-        width: _bannerAd!.size.width.toDouble(),
-        child: AdWidget(ad: _bannerAd!),
-      );
-    } else {
+    if (!_isLoaded || _bannerAd == null) {
       return const SizedBox.shrink();
     }
+
+    return SizedBox(
+      height: _bannerAd!.size.height.toDouble(),
+      width: _bannerAd!.size.width.toDouble(),
+      child: AdWidget(ad: _bannerAd!),
+    );
   }
 }
